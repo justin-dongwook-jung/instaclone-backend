ff5f4a3cf594fd3b6f7e75a8bd20f5916f54365f
justinjungkora <jungdw0624@gmail.com>
2021-12-04 02:05:50 +0900

    #4.13 Recap

--- users/seeProfile/seeProfile.resolvers.js
+++ users/seeProfile/seeProfile.resolvers.js
@@ -1,12 +1,13 @@
 import client from "../../client";
+import { protectedResolver } from "../users.utils";
 
 export default {
   Query: {
-    seeProfile: (_, { username }) =>
+    seeProfile: protectedResolver((_, { username }) =>
       client.user.findUnique({
         where: {
           username,
         },
-      }),
+      }))
   },
 };
