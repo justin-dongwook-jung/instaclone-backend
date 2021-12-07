35edb1776f10e681a9c64210256f9b9c9e323d22
justinjungkora <jungdw0624@gmail.com>
2021-12-07 17:07:06 +0900

    #4.27 Computed Fields part Two

--- users/users.resolvers.js
+++ users/users.resolvers.js
@@ -1,10 +1,28 @@
+import client from "../client";
+
 export default {
   User: {
-    totalFollowing: (root) => {
-      return root.following.length;
-    },
-    totalFollowers: (root) => {
-      return root.followers.length;
+    totalFollowing: ({id}) => client.user.count({
+      where:{
+        followers: {
+          some: {
+            id
+          }
+        }
+      }
+    }),
+    totalFollowers: ({id}) => client.user.count({
+      where: {
+        following: {
+          some: {
+            id
+          }
+        }
+      }
+    }),
+    isMe: ({id}, _, {loggedInUser}) => {
+      if(!loggedInUser) return false;
+      return id === loggedInUser.id;
     }
   }
 }
