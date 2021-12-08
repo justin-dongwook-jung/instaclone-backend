16a91129a544949d85b6e7b96968ea82e8284b87
justinjungkora <jungdw0624@gmail.com>
2021-12-08 14:14:06 +0900

    #6.3 Upload Photo part Two

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,12 +1,32 @@
+import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
   Mutation: {
     uploadPhoto: protectedResolver(
-      async (_, { file, aption }, { loggedInUser }) => {
+      async (_, { file, caption }, { loggedInUser }) => {
           if(caption){
-            
+            const hashtag = caption.match(/#[\w]+/g);
+            console.log(hashtag);
           }
+          client.photo.create({
+            data: {
+              file: file,
+              caption: caption,
+              hashtags: {
+                connectOrCreate: [
+                  {
+                    where: {
+                      hashtag: "#food"
+                    },
+                    create: {
+                      hashtag: "#food"
+                    }
+                  }
+                ]
+              }
+            }
+          })
       }
     )
   }
